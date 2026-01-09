77d0d216e61890a93276d5793a706df0c5fda3c9
Csécs Olivér Elemér <csecs.oliver.elemer@students.jedlik.eu>
2026-01-09 08:32:38 +0000

    create tables sql created

---
+++ init_db.sql
@@ -0,0 +1,54 @@
+CREATE TABLE users (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    email VARCHAR(320) UNIQUE,
+    passhash VARCHAR(32),
+    role INT
+);
+CREATE TABLE workshops (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    user_id INT,
+    created_at INT,
+    wdate INT DEFAULT -1,
+    wlocation VARCHAR(255) DEFAULT '',
+    max_participants INT DEFAULT -1,
+    sponsor VARCHAR(255) DEFAULT 'none',
+    visibility INT,
+    extra_text VARCHAR(3200) DEFAULT '',
+    extra_text_visibility INT DEFAULT 0
+);
+CREATE TABLE files (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    workshop_id INT,
+    fpath VARCHAR(255)
+);
+CREATE TABLE participations (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    workshop_id INT,
+    user_id INT,
+    approved INT DEFAULT 0,
+    pname VARCHAR(255),
+    notes VARCHAR(3200) DEFAULT ''
+);
+CREATE TABLE email_codes (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    code VARCHAR(255),
+    user_id INT
+);
+CREATE TABLE session_tokens (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    token VARCHAR(64),
+    user_id INT,
+    expiry INT
+);
+CREATE TABLE invites ( -- these are generic invites, not tied to specific users
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    workshop_id INT,
+    code VARCHAR(6),
+    uses_left INT DEFAULT 1
+);
+
+CREATE TABLE claimed_invites ( -- these are invites that have been claimed by specific users
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    workshop_id INT,
+    user_id INT
+);
